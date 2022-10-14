7faf9a71e3df33761d89b01e453e14d2b73102a2
rajat4885 <100342306+rajat4885@users.noreply.github.com>
2022-10-15 01:54:44 +0530

    y

---
+++ Themes/corporate/ui/js/retail-investors-initca4c.js
@@ -0,0 +1,58 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const triggerButton = document.querySelector('[data-epiforms-metadata="ir_subscription_trigger"] [type="submit"]');
+    if (triggerButton)
+        triggerButton.classList.remove('FormSubmitButton');
+
+    handleSubscriptionValidation();
+
+    function handleSubscriptionValidation() {
+        const subscriptionForm = document.querySelector('[data-epiforms-metadata="ir_subscription"]');
+        if (!subscriptionForm) return;
+
+        const subscriptionSubmit = subscriptionForm.querySelector('.FormSubmitButton');
+        if (!subscriptionSubmit) return;
+
+        subscriptionSubmit.addEventListener('click', function (event) {
+            const choiceElems = subscriptionForm.querySelectorAll('.FormChoice');
+            if (!choiceElems || choiceElems.length === 0) return;
+
+            for (const el of choiceElems) {
+                const groupData = el.querySelector('.js-radio-elements');
+                if (!groupData) continue;
+
+                const groupElems = groupData.getAttribute('data-radio-elements').split(',');
+                const groupRequired = groupData.getAttribute('data-group-required');
+                const requiredMsg = groupData.getAttribute('data-required-message');
+                if (!groupElems || groupElems.length === 0) continue;
+                if (groupRequired.toLowerCase() !== 'true') continue;
+
+                let arr = [];
+                for (const groupElem of groupElems) {
+                    const e = subscriptionForm.querySelector(`.FormChoice [data-name=${groupElem}]`);
+                    if (!e) continue;
+
+                    arr.push(e);
+                }
+
+                let selected = false;
+                for (const e of arr) {
+                    if (e.checked) {
+                        selected = true;
+                        break;
+                    }
+                }
+
+                const errorMsg = el.querySelector('.Form__Element__ValidationError');
+                if (!selected) {
+                    errorMsg.style.display = 'block';
+                    errorMsg.innerHTML = requiredMsg;
+                    event.preventDefault();
+                    event.stopImmediatePropagation();
+                } else {
+                    errorMsg.style.display = 'none';
+                    errorMsg.innerHTML = '';
+                }
+            }
+        });
+    }
+});
